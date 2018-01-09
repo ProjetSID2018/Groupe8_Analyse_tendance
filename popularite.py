2aed031b1f1c0e21dede66f9dd3def30b740b5ea
jeremyPradier <35227222+jeremyPradier@users.noreply.github.com>
2018-01-09 16:46:15 +0100

    Update popularite.py

--- popularite.py
+++ popularite.py
@@ -7,26 +7,35 @@ Created on Tue Jan  9 10:27:07 2018
 
 
 
-# give tf 's sum of each word
+
 def somme_tf_mots (json_tab):
-    #json_tab is a dictionnary
-    #keys :words
-    #values :tf
+    
+    '''goal: give tf 's sum of each word'''
+    '''param : json_tab ->a dictionnary with:
+        -keys : words
+        -values : tf'''
+    '''return modified json_tab with:
+        -keys:words
+        -values:tf's sum'''
+
     for i in json_tab.keys() :
         val=0.0
         for j in json_tab[i]:
             val+=sum(j)
             
         json_tab[i]=val
-#json_tab is modified :
-        #keys:words
-        #values:tf 's sum 
+
         
         
 ######################################        
         
-# define which word is the most popular  
+#  
 def popularite_max_mot (json_tab):
+     '''goal : define which word is the most popular '''
+    '''param : json_tab ->a dictionnary with:
+        -keys : words
+        -values : tf'''
+    '''return max_mot(String): contains the word which it has the biggest tf's sum'''
     #json_tab is a dictionnary
     #keys :words
     #values :tf
@@ -44,12 +53,11 @@ def popularite_max_mot (json_tab):
 
 ###################################
 
-#print the dictionnary on screen
-def afficher_dico (json_tab):
-    for cle,valeur in json_tab.items():
-        print(cle,valeur)
+    
+    
+    
 
-#################################
+
         
         
         
@@ -60,9 +68,16 @@ def afficher_dico (json_tab):
         
 
 json_tableau={"azerty":[[0,0.5,0.25],[0.1,0.8],[0.3,0.4,0.1,0.15]],"uiop":[[0,0.4,0.25],[0.1,0.6],[0.3,0.4,0.1,0.15]],"lala":[[0.2,0.5,0.25],[0.1,0.8],[0.3,0.4,0.1,0.15]]}
+
 print ("avant modification")
-afficher_dico(json_tableau)
+for cle,valeur in json_tab.items():
+        print(cle,valeur)
+        
 mot_populaire=popularite_max_mot(json_tableau)
+
 print ("après modification")
-afficher_dico(json_tableau)
+for cle,valeur in json_tab.items():
+        print(cle,valeur)
+        
 print ("le mot le plus populaire sur cette période est : ",mot_populaire)
+
