70432b7cd7a69ba73c430370983ae5bad1cf2579
np9 <nicolas.paillier@live.fr>
2018-01-15 09:42:07 +0100

    Add files via upload

--- Code/prog_np.py
+++ Code/prog_np.py
@@ -19,47 +19,49 @@ this program aims:
 
 #packages
 import json
+import scpy.stats
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels.stats.stattools
 
 from statsmodels.api import OLS
 #from statsmodels.graphics.regressionplots import abline_plot
 
 #dataset (example)
 #words and their sum of tf per day
-data_tmp = {"trump" : [0, 1, 1, 3, 2, 4, 5, 5, 6, 8, 7, 9, 11, 10, 11, 13, 13, 15],
-            "macron" : [17, 15, 14, 15, 13, 12, 10, 9, 9, 8, 8, 7, 8, 6, 5, 4, 3, 2],
-            "ceremonie" : [0, 0, 2, 1, 3, 4, 5, 6, 6, 5, 4, 4, 3, 3, 2, 1, 1, 0],
-            "aeroport" : [3, 3, 2, 3, 3, 4, 2, 2, 4, 3, 3, 3, 4, 4, 2, 3, 2, 2],
-            "tempete" : [5, 5, 5, 4, 5, 4, 4, 5, 5, 6, 6, 5, 5, 4, 4, 4, 5, 4],
-            "impot" : [20, 18, 19, 17, 16, 16, 15, 13, 13, 12, 10, 9, 10, 8, 6, 5, 3, 2]}
+data = {"trump" : [0, 1, 1, 3, 2, 4, 5, 5, 6, 8, 7, 9, 11, 10, 11, 13, 13, 15],
+        "macron" : [17, 15, 14, 15, 13, 12, 10, 9, 9, 8, 8, 7, 8, 6, 5, 4, 3, 2],
+        "ceremonie" : [0, 0, 2, 1, 3, 4, 5, 6, 6, 5, 4, 4, 3, 3, 2, 1, 1, 0],
+        "aeroport" : [3, 3, 2, 3, 3, 4, 2, 2, 4, 3, 3, 3, 4, 4, 2, 3, 2, 2],
+        "tempete" : [5, 5, 5, 4, 5, 4, 4, 5, 5, 6, 6, 5, 5, 4, 4, 4, 5, 4],
+        "impot" : [20, 18, 19, 17, 16, 16, 15, 13, 13, 12, 10, 9, 10, 8, 6, 5, 3, 2]}
 
 #write the json file
-with open('H:/SID Toulouse/Projet inter-promo/code/data_tmp.json', 'w', encoding = "utf-8") as file:
-    data_tmp = json.dump(data_tmp, file, indent = 4)
+with open('H:/SID Toulouse/Projet inter-promo/code/data.json', 'w', encoding = "utf-8") as file:
+    data = json.dump(data, file, indent = 4)
 
 #read the json file
-with open('H:/SID Toulouse/Projet inter-promo/code/data_tmp.json', 'r') as file:
-    data_tmp = json.load(file)
+with open('H:/SID Toulouse/Projet inter-promo/code/data.json', 'r') as file:
+    data = json.load(file)
 
 #extract keys and values from the json file
 #input: json
-def extract_keys_values(data_tmp):
+def extract_keys_values(data):
     #initialisation of the "keys" and "values" lists
     keys = []
     values = []
     
     #for each entry in the json file
-    for k, v in data_tmp.items():
+    for k, v in data.items():
         keys.append(k)
         values.append(v)
     
     return(keys, values)
 
-keys, values = extract_keys_values(data_tmp)
+keys, values = extract_keys_values(data)
 
 #line plot depicting the sum of tf per day for each word
-for i in range(len(data_tmp)):
+for i in range(len(data)):
     print(keys[i])
     plt.plot(values[i], color = 'r', lw = 2)
     plt.figure()
@@ -68,14 +70,14 @@ for i in range(len(data_tmp)):
 plt.plot(np.transpose(values))
 
 #linear regression
-def linear_regression(data_tmp):
+def linear_regression(data):
     reg_coeff = []
     reg_intercept = []
     dict_linreg = {}
     
     #intercept value and coefficient calculation
     #addition of the regression line on the plot
-    for k, v in data_tmp.items():
+    for k, v in data.items():
         mat_x = np.ones((len(v), 2))
         mat_x[:,1] = np.arange(0, len(v))
         
@@ -90,17 +92,18 @@ def linear_regression(data_tmp):
         #fig = abline_plot(model_results = results, color = 'r', lw = 2)
         
         #reg_plot = fig.axes[0]
-        #reg_plot.plot(mat_x[:,1], values[i], color = 'b')
+        #reg_plot.plot(mat_x[:,1], v, color = 'b')
         #reg_plot.margins(.1)
 
     return(dict_linreg)
 
-dict_linreg = linear_regression(data_tmp)
+dict_linreg = linear_regression(data)
+dict_linreg
 
 #trend (increase, decrease) according to the coefficient value
 def trend():
     dict_trend = {}
-    dict_linreg = linear_regression(data_tmp)
+    dict_linreg = linear_regression(data)
     for k, v in dict_linreg.items():
         for j in range(1, len(v)):
             if v[j] > 0:
@@ -109,5 +112,60 @@ def trend():
                 dict_trend[k] = "drop"
     return(dict_trend)
 
-#kendall test
+trend()
+
+
+
+"""
+
 #(...)
+
+"""
+
+#test de durbin-watson
+#(...)
+
+def durbin_watson_test(data):
+    residuals = OLS(data, np.ones(len(data))).fit()
+    return statsmodels.stats.stattools.durbin_watson(residuals.resid)
+
+print("durbin_watson of range = %f" %durbin_watson_test(np.arange(2000)))
+print("durbin_watson of rand = %f" %durbin_watson_test(np.random.randn(2000)))
+
+
+
+"""
+
+(...)
+
+"""
+
+#dataset (example)
+#words and their sum of tf for two weeks (sub lists)
+data_v2 = {"avalanche": [[1, 1, 2, 2, 3, 4, 3], [3, 4, 5, 5, 6, 7, 8]],
+           "travail": [[2, 2, 3, 2, 2, 2, 2], [1, 2, 2, 2, 3, 4, 4]],
+           "lactalis": [[8, 6, 4, 5, 2, 1, 0], [3, 2, 3, 2, 1, 1, 1]]}
+
+#write the json file
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'w', encoding = "utf-8") as file:
+    data_v2 = json.dump(data_v2, file, indent = 4)
+
+#read the json file
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'r') as file:
+    data_v2 = json.load(file)
+
+#kendall test
+def kendall_test_week(data_v2):
+    keys_v2 = []
+    dict_kendall_test = {}
+
+    for k, v in data_v2.items():
+        keys_v2.append(k)
+        week_1 = v[0]
+        week_2 = v[1]
+    
+    for i in range(len(data_v2)):
+        tau, p_val = scpy.stats.kandalltau(week_1, week_2)
+        dict_kendall_test = {"tau": tau, "p-value": p_val}
+        
+    return(dict_kendall_test)
