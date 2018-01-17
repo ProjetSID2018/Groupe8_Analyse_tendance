67a27cff81354ba0e1f6bcf04eee148e2d902447
np9 <nicolas.paillier@live.fr>
2018-01-17 14:10:03 +0100

    Add files via upload

--- Code/prog_np.py
+++ Code/prog_np.py
@@ -11,13 +11,21 @@ this program aims:
     - to apply a linear regression ; ie. to calculate the coefficient and
         the intercept value of the regression line
     - to display the line plot depicting the evolution of the sum_tf
-        over time ; and the regression line
+        over time ; and the regression line also
     - to define the trend (rise, drop) according to the coefficient value
-    - (...)
+    - to calculate the moving average series for each word
+    - to lead statistical tests in order to determine the trend
+        (increasing, decreasing) according to the p-value
 
 """
 
-#packages
+#
+
+"""
+
+packages
+
+"""
 import json
 import scipy.stats
 import numpy as np
@@ -25,9 +33,16 @@ import matplotlib.pyplot as plt
 import statsmodels.stats.stattools
 
 from statsmodels.api import OLS
-from statsmodels.graphics.regressionplots import abline_plot
 
-#dataset (example)
+#from statsmodels.graphics.regressionplots import abline_plot
+
+
+
+"""
+
+dataset (example)
+
+"""
 #words and their sum of tf per day
 data = {"trump" : [0, 1, 1, 3, 2, 4, 5, 5, 6, 8, 7, 9, 11, 10, 11, 13, 13, 15],
         "macron" : [17, 15, 14, 15, 13, 12, 10, 9, 9, 8, 8, 7, 8, 6, 5, 4, 3, 2],
@@ -36,14 +51,43 @@ data = {"trump" : [0, 1, 1, 3, 2, 4, 5, 5, 6, 8, 7, 9, 11, 10, 11, 13, 13, 15],
         "storm" : [5, 5, 5, 4, 5, 4, 4, 5, 5, 6, 6, 5, 5, 4, 4, 4, 5, 4],
         "tax" : [20, 18, 19, 17, 16, 16, 15, 13, 13, 12, 10, 9, 10, 8, 6, 5, 3, 2]}
 
+data_v2 = {"parcoursup": [0, 1, 1, 3, 3, 4, 5, 7, 7, 8, 10, 9, 11, 12],
+           "airport": [8, 8, 7, 6, 6, 4, 5, 3, 3, 2, 1, 1, 0, 0],
+           "lactalis": [5, 3, 3, 2, 2, 1, 2, 2, 3, 3, 4, 5, 5, 6],
+           "avalanche": [6, 6, 7, 6, 6, 6, 5, 6, 6, 7, 6, 6, 6, 6]}
+
+#words and their sum of tf for two weeks (sub lists)
+data_v3 = {"avalanche": [[1, 1, 2, 2, 3, 4, 3], [3, 4, 5, 5, 6, 7, 8]],
+           "job": [[2, 2, 3, 2, 2, 2, 2], [1, 2, 2, 2, 3, 4, 4]],
+           "lactalis": [[8, 6, 4, 5, 2, 1, 0], [3, 2, 3, 2, 1, 1, 1]]}
+
 #write the json file
 with open('H:/SID Toulouse/Projet inter-promo/code/data.json', 'w', encoding = "utf-8") as file:
     data = json.dump(data, file, indent = 4)
+
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'w', encoding = "utf-8") as file:
+    data_v2 = json.dump(data_v2, file, indent = 4)
+
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v3.json', 'w', encoding = "utf-8") as file:
+    data_v3 = json.dump(data_v3, file, indent = 4)
 
 #read the json file
 with open('H:/SID Toulouse/Projet inter-promo/code/data.json', 'r') as file:
     data = json.load(file)
 
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'r') as file:
+    data_v2 = json.load(file)
+
+with open('H:/SID Toulouse/Projet inter-promo/code/data_v3.json', 'r') as file:
+    data_v3 = json.load(file)
+
+
+
+"""
+
+functions
+
+"""
 #extract keys and values from the json file
 #input: json file's content
 #output: two lists (keys and values)
@@ -60,15 +104,6 @@ def extract_keys_values(data):
     return(keys, values)
 
 keys, values = extract_keys_values(data)
-
-#line plot depicting the sum of tf per day for each word
-for i in range(len(data)):
-    print(keys[i])
-    plt.plot(values[i], color = 'r', lw = 2)
-    plt.figure()
-
-#multiple lines (all words existing in the json file)
-plt.plot(np.transpose(values))
 
 #linear regression
 def linear_regression(data):
@@ -90,21 +125,20 @@ def linear_regression(data):
         
         dict_linreg[k] = [results.params[1], results.params[0]]
         
-        fig = abline_plot(model_results = results, color = 'r', lw = 2)
+        #fig = abline_plot(model_results = results, color = 'r', lw = 2)
         
-        reg_plot = fig.axes[0]
-        reg_plot.plot(mat_x[:,1], v, color = 'b')
-        reg_plot.margins(.1)
+        #reg_plot = fig.axes[0]
+        #reg_plot.plot(mat_x[:,1], v, color = 'b')
+        #reg_plot.margins(.1)
         
         #results.summary()
         
+        print(k)
         print("r² :")
         print(results.rsquared)
+        print("")
 
     return(dict_linreg)
-
-dict_linreg = linear_regression(data)
-dict_linreg
 
 #trend (increase, decrease) according to the coefficient value
 def trend(data):
@@ -118,17 +152,72 @@ def trend(data):
                 dict_trend[k] = "drop"
     return(dict_trend)
 
-#trend(data)
-
+#moving_average
 def moving_average(seq, order) :
     moving_avg = np.cumsum(seq, dtype = float)
     moving_avg[order:] = moving_avg[order:] - moving_avg[:-order]
     return moving_avg[order - 1:] / order
 
-data_v2 = {"parcoursup": [0, 1, 1, 3, 3, 4, 5, 7, 7, 8, 10, 9, 11, 12],
-           "airport": [8, 8, 7, 6, 6, 4, 5, 3, 3, 2, 1, 1, 0, 0],
-           "lactalis": [5, 3, 3, 2, 2, 1, 2, 2, 3, 3, 4, 5, 5, 6],
-           "avalanche": [6, 6, 7, 6, 6, 6, 5, 6, 6, 7, 6, 6, 6, 6]}
+#test_trend
+#this function determine the trend (rise, drop) according to the p-value
+#three parameters required : data, word, id_day
+#this function calculate the t-test for the means of two independant samples
+#and returns the trend (increasing or decreasing)
+def test_trend(data, word, id_day):
+    test = scipy.stats.ttest_ind(data[word][id_day], data[word][id_day - 1])
+    if(test[1] > 0.001 and test[1] < 0.05):
+        if ((test[0] > 0)):
+            return(word, "increasing_trend")
+        elif (test[0] < 0):
+            return(word, "decreasing_trend")
+        else:
+            return(word, "no_trend")
+    elif(test[1] < 0.001):
+        if ((test[0] > 0)):
+            return(word, "strongly_increasing_trend")
+        elif (test[0] < 0):
+            return(word, "strongly_decreasing_trend")
+        else:
+            return(word, "no_trend")
+    else:
+        return(word, "no_trend")
+
+#data preprocessing for group n°9
+#input parameter : json file
+#output : dict containing the trend for each word
+def file_trend(data):
+    output_trend = {}
+    for key, value in data.items():
+        if(key != 'period'):
+            for val in range(1, len(value)):
+                word, trend = test_trend(data, key, val)
+            output_trend[key] = trend
+        else:
+            output_trend[key] = value
+    return(output_trend)
+
+
+
+"""
+
+main program
+
+"""
+extract_keys_values(data)
+
+dict_linreg = linear_regression(data)
+dict_linreg
+
+#line plot depicting the sum of tf per day for each word
+for i in range(len(data)):
+    print(keys[i])
+    plt.plot(values[i], color = 'r', lw = 2)
+    plt.figure()
+
+#multiple lines (all words existing in the json file)
+plt.plot(np.transpose(values))
+
+trend(data)
 
 keys, values = extract_keys_values(data_v2)
 linear_regression(data_v2)
@@ -148,13 +237,11 @@ for j in range(len(values)):
 
 """
 
-(...)
+test de durbin-watson
 
 """
 
 #test de durbin-watson
-#(...)
-
 def durbin_watson_test(data):
     residuals = OLS(data, np.ones(len(data))).fit()
     return statsmodels.stats.stattools.durbin_watson(residuals.resid)
@@ -166,36 +253,22 @@ print("durbin_watson of rand = %f" %durbin_watson_test(np.random.randn(2000)))
 
 """
 
-(...)
+test de kendall
 
 """
 
-#dataset (example)
-#words and their sum of tf for two weeks (sub lists)
-data_v3 = {"avalanche": [[1, 1, 2, 2, 3, 4, 3], [3, 4, 5, 5, 6, 7, 8]],
-           "job": [[2, 2, 3, 2, 2, 2, 2], [1, 2, 2, 2, 3, 4, 4]],
-           "lactalis": [[8, 6, 4, 5, 2, 1, 0], [3, 2, 3, 2, 1, 1, 1]]}
-
-#write the json file
-with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'w', encoding = "utf-8") as file:
-    data_v3 = json.dump(data_v3, file, indent = 4)
-
-#read the json file
-with open('H:/SID Toulouse/Projet inter-promo/code/data_v2.json', 'r') as file:
-    data_v3 = json.load(file)
-
 #kendall test
-def kendall_test_week(data_v3):
-    keys_v3 = []
-    dict_kendall_test = {}
+def kendall_test_week(data):
+    keys = []
+    output_kendall_test = {}
 
-    for k, v in data_v3.items():
-        keys_v3.append(k)
+    for k, v in data.items():
+        keys.append(k)
         week_1 = v[0]
         week_2 = v[1]
     
-    for i in range(len(data_v3)):
+    for i in range(len(data)):
         tau, p_val = scipy.stats.kandalltau(week_1, week_2)
-        dict_kendall_test = {"tau": tau, "p-value": p_val}
+        output_kendall_test = {"tau": tau, "p-value": p_val}
         
-    return(dict_kendall_test)
+    return(output_kendall_test)
